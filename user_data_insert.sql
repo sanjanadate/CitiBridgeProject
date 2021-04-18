1322d5c55ae898216e078d4aa368da155668338f
sanjanadate <68459515+sanjanadate@users.noreply.github.com>
2021-04-18 13:53:24 +0530

    User_data insert script

---
+++ user_data_insert.sql
@@ -0,0 +1,9 @@
+USE `yahoofinance` ; 
+
+insert  into user_data values 
+(1, 'Rahul Shukla', 'Rahul123'),
+(2, 'Ashmi Singh', 'Ashmi123'),
+(3, 'Yash Sharma', 'Yash123'),
+(4, 'Jay Joshi', 'Jay123'),
+(5, 'Rani Saxena', 'Rani123'),
+(6, 'Namrata Ketkar', 'Namrata123');
