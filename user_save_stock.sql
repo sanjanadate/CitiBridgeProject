d1df8b15ba85bccbbdfc60823f659c863d409872
sanjanadate <68459515+sanjanadate@users.noreply.github.com>
2021-04-23 22:50:55 +0530

    Add files via upload

---
+++ user_save_stock.sql
@@ -0,0 +1,10 @@
+USE `yahoofinance` ; 
+
+insert into user_saved_stock values 
+(1, 'ADANIPORTS.NS', 100.5,1),
+(1, 'AXISBANK.NS', 50,2),
+(1, 'INFY.NS', 75,1),
+(1, 'NESTLEIND.NS', 200.5,2),
+(1, 'UPL.NS', 104.5,1),
+(2, 'ADANIPORTS.NS', 100.5,2),
+(2, 'AXISBANK.NS', 50,1);
